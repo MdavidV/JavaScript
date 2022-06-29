0ce3395cba1818f2515234663d874bda44e7c2e4
David V <vinaarizadavid@gmail.com>
2022-06-28 20:50:55 -0500

    Segunda Entrega Proyecto GyV Shoes

--- Desafio Final/js/main.js
+++ Desafio Final/js/main.js
@@ -1,122 +1,63 @@
+"use strict";
+
 const BBDD = [
-    product1 = {
-        id: 1,
-        name: 'product1',
-        price: 100,
-        amount: 0
-
-    },
-    product2 = {
-        id: 2,
-        name: 'product2',
-        price: 100,
-        amount: 0
-
-    },
-    product3 = {
-        id: 3,
-        name: 'product3',
-        price: 100,
-        amount: 0
-
-    },
-    product4 = {
-        id: 4,
-        name: 'product4',
-        price: 100,
-        amount: 0
-
-    },
-    product5 = {
-        id: 5,
-        name: 'product5',
-        price: 100,
-        amount: 0
-
-    },
-    product6 = {
-        id: 6,
-        name: 'product6',
-        price: 100,
-        amount: 0
-
-    },
-    product7 = {
-        id: 7,
-        name: 'product7',
-        price: 100,
-        amount: 0
-
-    },
-    product8 = {
-        id: 8,
-        name: 'product8',
-        price: 100,
-        amount: 0
-
-    },
-    product9 = {
-        id: 9,
-        name: 'product9',
-        price: 100,
-        amount: 0
-
-    }
+  {
+    id: 1,
+    position: "p1",
+    nombre: "Zapato Epico 01",
+    img: "assets/1.png",
+    precio: 80,
+    cantidad: 1,
+  },
+  {
+    id: 2,
+    position: "p2",
+    nombre: "Zapato Epico 02",
+    img: "assets/2.png",
+    precio: 90,
+    cantidad: 1,
+  },
+  {
+    id: 3,
+    position: "p3",
+    nombre: "Zapato Epico 03",
+    img: "assets/3.png",
+    precio: 100,
+    cantidad: 1,
+  },
+  {
+    id: 4,
+    position: "p4",
+    nombre: "Zapato Epico 04",
+    img: "assets/4.png",
+    precio: 110,
+    cantidad: 1,
+  },
+  {
+    id: 5,
+    position: "p5",
+    nombre: "Zapato Epico 05",
+    img: "assets/5.png",
+    precio: 120,
+    cantidad: 1,
+  },
+  {
+    id: 6,
+    position: "p6",
+    nombre: "Zapato Epico 06",
+    img: "assets/7.jpg",
+    precio: 130,
+    cantidad: 1,
+  },
+  {
+    id: 7,
+    position: "p7",
+    nombre: "Zapato Epico 07",
+    img: "assets/8.jpg",
+    precio: 140,
+    cantidad: 1,
+  },
 ];
-let cart = [];
-let cartPrice = [];
-var reducer = (accumulator, curr) => accumulator + curr;
-
-function addToCart(id) {
-    let product = prompt(`
-        Select a product : 
-        1 - 9 
-    `);
-    parseInt(product);
-
-    if (product == 1 || product == 2 || product == 3 || product == 4 || product == 5 || product == 6 || product == 7 || product == 8 || product == 9) {
-        id = product;
-    }else{
-        alert(`
-            Pleashe Choose A valid Option.
-        `)
-    }
-
-
-
-
-
-    let producto = BBDD.find(producto => producto.id == id);
-    cart.push(producto);
-
-    let amount = parseInt(prompt(`
-        How many shoes do you want?
-        1 - 10
-    `));
-
-    let totalAmount = producto.amount = amount;
-    let totalPrice = totalAmount * producto.price;
-    cartPrice.push(totalPrice);
-    let totalResult = cartPrice.reduce(reducer);
-    console.log(totalResult);
-
-
-
-    let buyAgain = prompt(`
-        Do you Want to Buy Again? 
-        1: yes
-        2: no
-    `);
-
-    if (buyAgain == 1) {
-        addToCart();
-    }else{
-        alert(`
-            Total Price: ${totalResult};
-        `)
-    }
-
-}
 
 
 
---
+++ Desafio Final/js/test.js
@@ -0,0 +1,128 @@
+"use strict";
+
+// AGARRAR ELEMENTOS DEL DOM
+
+const cartContainer = document.querySelector("#cartContainer");
+const cartOpener = document.querySelector("#cartIcon");
+const cartCloser = document.querySelector(".closer");
+const mainCartCloser = document.querySelector("#closeCart");
+const totalPrice = document.querySelector("#totalPrice");
+
+// Carrito
+let cart;
+const cartLs = JSON.parse(localStorage.getItem("cart"));
+
+renderProducts(".men-products-container");
+renderProducts(".women-products-container");
+
+const openCart = () => {
+  const cartCont = document.querySelector("#cart");
+  const menCont = document.querySelector(".men-products");
+  const womenCont = document.querySelector(".women-products");
+
+  menCont.classList.add("hidden");
+  womenCont.classList.add("hidden");
+  cartCont.classList.remove("hidden");
+  cartCont.classList.add("visible");
+};
+
+const closeCart = () => {
+  const cartCont = document.querySelector("#cart");
+  const menCont = document.querySelector(".men-products");
+  const womenCont = document.querySelector(".women-products");
+
+  menCont.classList.remove("hidden");
+  womenCont.classList.remove("hidden");
+  cartCont.classList.add("hidden");
+  cartCont.classList.remove("visible");
+};
+
+function renderProducts(idDelContenedor) {
+  const container = document.querySelector(idDelContenedor);
+
+  BBDD.forEach((product) => {
+    let productHTML = `
+    <div class="card" id="${product.position}">
+        <img src="${product.img}" class="slider-img" />
+        <div class="card-details">
+          <h2 class="product-name">${product.nombre}</h2>
+          <p class="slider-review">${product.precio}$</p>
+        <button class="btn-send-card" id="${product.id}" onclick="addToCart(${product.id})">Add to Cart</button>
+      </div>
+    </div>
+    `;
+    container.innerHTML += productHTML;
+  });
+}
+
+const addToCart = (id) => {
+  let product = BBDD.find((item) => item.id === id);
+  cart.push(product);
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+
+  renderCart();
+  rederTotal();
+};
+
+const removeCart = (id) => {
+  const item = cart.find((product) => product.id === id);
+  const index = cart.indexOf(item);
+
+  cart.splice(index, 1);
+  localStorage.setItem("cart", JSON.stringify(cart));
+
+  renderCart();
+  rederTotal();
+};
+
+const renderCart = () => {
+  cartContainer.innerHTML = "";
+
+  cart.forEach((item) => {
+    const div = document.createElement("div");
+    div.classList.add("product-in-cart");
+
+    div.innerHTML = `
+    <figure class="cart-product-img">
+      <img src="${item.img}" alt="">
+    </figure>
+
+    <p>${item.nombre}</p>
+    <p>${item.precio}$</p>
+
+    <button onclick="removeCart(${item.id})" class="btn-delete">
+      <i class="fas fa-trash-alt"></i>
+    </button>
+    `;
+
+    cartContainer.append(div);
+  });
+};
+
+const rederTotal = () => {
+  let total = 0;
+
+  cart.forEach((product) => {
+    total += product.precio;
+  });
+
+  totalPrice.innerText = total;
+};
+
+//  EVENTOS
+
+cartOpener.addEventListener("click", openCart);
+cartCloser.addEventListener("click", closeCart);
+mainCartCloser.addEventListener("click", closeCart);
+
+// CONDICIONAL DEL LOCAL STORAGE
+
+if (cartLs) {
+  cart = cartLs;
+
+  renderCart();
+  rederTotal();
+} else {
+  cart = [];
+}
